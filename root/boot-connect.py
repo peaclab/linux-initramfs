02fe40543486173fd083473440e64c574330b298
Allen Zou <allenzou@bu.edu>
2021-06-23 16:35:31 -0400

    added git keeps

--- root/boot-connect.py
+++ root/boot-connect.py
@@ -1,10 +1,10 @@
 import usocket as socket
 
 s = socket.socket()
-ai = socket.getaddrinfo('104.21.25.187', 80)[0][-1]
+ai = socket.getaddrinfo('192.168.1.200', 80)[0][-1]
 s.connect(ai)
 
-s.send(b"GET /200 HTTP/1.0\r\nHost: 104.21.25.187\r\n\r\n")
+s.send(b"GET /200 HTTP/1.0\r\nHost: 192.168.1.200\r\n\r\n")
 print(s.recv(4096))
 with open("/proc/uptime", 'r') as u:
     print(u.read())
