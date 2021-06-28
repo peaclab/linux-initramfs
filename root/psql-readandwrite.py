c11f8afb605f4fca9eb3722b6ab5050377da3957
Allen Zou <allenzou@bu.edu>
2021-06-28 15:34:26 -0400

    changed the ip of the psql script to reflect our managed switch

--- root/psql-readandwrite.py
+++ root/psql-readandwrite.py
@@ -1,13 +1,13 @@
 import micropg as p
 
-conn=p.connect(user="postgres", password="postgres", host="128.197.176.151", port="5432", database="dvdrental")
+conn=p.connect(user="postgres", password="postgres", host="192.168.1.156", port="5432", database="dvdrental")
 cur = conn.cursor()
 cur.execute('select title from film')
 
 allMovies = cur.fetchall()
 #cur.execute('truncate table selectedMovies restart identity')
 for movie in allMovies:
-        print("looking at", movie[0])
+#        print("looking at", movie[0])
         if (movie[0][0] == 'B'):
                 cur.execute('insert into selectedMovies(name) values(\''+movie[0]+'\')')
 conn.commit()
