f9aa78732f0ddbcb2e4b944e369f81e81ec5d80f
Allen Zou <allenzou@bu.edu>
2021-06-28 14:59:02 -0400

    created new read and write script to edit the psql server

--- root/db.py
+++
@@ -1,7 +0,0 @@
-import micropg as p
-
-conn=p.connect(user="postgres", password="postgres", host="128.197.176.151", port="5432", database="dvdrental")
-cur = conn.cursor()
-cur.execute('select title from film')
-print(cur.fetchall())
-cur.close()
